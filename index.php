22144985190523a096496606e94da35ef60761c5
sendyapriatna <sendyapriatna001@gmail.com>
2021-04-01 12:21:21 +0700

    my port

---
+++ index.php
@@ -0,0 +1,302 @@
+<html lang="en" id="home">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="icon" type="icon/png" href="img/pika.ico">
+
+    <title>Sendy Apriatna</title>
+  </head>
+  <body>
+
+  	<!-- Navbar -->
+  	<nav class="navbar navbar-expand-sm navbar-dark fixed-top scrolling-navbar" style="background-color: rgba(34, 36, 30, 0.5);">
+  		<div class="container">
+  			<div class="navbar-header">
+  				<a class="navbar-brand page-scroll" href="#home" style="">The Smurf.</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+  			</div>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      			<ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link page-scroll" href="#about">about</a></li>
+                    <li class="nav-item"><a class="nav-link page-scroll" href="#skill">skill</a></li>
+                    <li class="nav-item"><a class="nav-link page-scroll" href="#services">services</a></li>
+    				<li class="nav-item"><a class="nav-link page-scroll" href="#portfolio">portfolio</a></li>
+    				<li class="nav-item"><a class="nav-link page-scroll" href="#contact">contact</a></li>
+    			    <li class="nav-item"><a class="nav-link page-scroll" href="#more">more</a></li>
+    			</ul>
+            </div>
+  		</div>
+		</nav>
+    <!-- End Navbar -->
+
+    <!-- Header -->
+    <div class="header">
+        <div class="container">
+            <div class="row justify-content-center" >
+                <div class="col-sm-6 align-self-center" style="padding-top: 140px;">
+                    <h1 style="font-size: 40px" class="font-weight-bold">Hi, I'm Sendy Apriatna</h1>
+                    <h1 style="font-size: 60px" class="font-weight-bold">Expert Graphic Designer</h1>
+                    <button class="cusbutton" type="submit" style="width: 100px;">HIRE ME</button>
+                </div>
+                <div class="col-sm-6 align-self-center">
+                    <img class="img-fluid" src="img/me/about2.png">
+                </div>
+            </div>  
+        </div>
+    </div>
+    <!-- End Header -->
+
+     <!-- About -->
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-6"></div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-2 text-center">
+                    <h5>ABOUT ME</h5>
+                    <hr class="line">
+                    <br>
+                </div>
+                <div class="col-sm-2"></div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-6">
+                    <img class="img-fluid" src="img/me/about.png">
+                </div>
+                <div class="col-sm-6">
+                    <p style="text-align: justify;">
+                        Hello, My name is Sendy Apriatna. I am a Expert Designer. My experience has taught me that good design is not created in a moment of inspiration or a bubble. It is an iterative process rquiring collaboration between multiple teams and your end user.
+                    </p>
+                    <button class="cusbutton" type="submit">DOWNLOAD CV</button>
+                </div>
+            </div>
+        </div>  
+    </section>
+    <!-- End About -->
+
+    <!-- Skill -->
+    <section class="skill" id="skill">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-2 text-center">
+                    <h5>MY SKILLS</h5>
+                    <hr class="line">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-3 text-center">
+                    <div class="cc">
+                        <img src="img/skill/ps3.png">
+                        <p>77%</p>
+                    </div>
+                    <p>Adobe Photoshop</p>
+                </div>
+                <div class="col-sm-3 text-center">
+                    <div class="cc">
+                        <img src="img/skill/ps.png">
+                        <p>56%</p>
+                    </div>
+                    <p>Adobe Premiere</p>
+                </div>
+                <div class="col-sm-3 text-center">
+                    <div class="cc">
+                        <img src="img/skill/ai.png">
+                        <p>45%</p>
+                    </div>
+                    <p>Adobe Ilustrator</p>
+                </div>
+                <div class="col-sm-3 text-center">
+                    <div class="cc">
+                        <img src="img/skill/ps2.png">
+                        <p>63%</p>
+                    </div>
+                    <p>Corel Draw</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Skill -->
+
+    <!-- Services -->
+    <section class="services" id="services">
+        <div class="container"> 
+            <div class="row justify-content-center">
+                <div class="col-sm-2 text-center">
+                    <h5>SERVICES</h5>
+                    <hr class="line">
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-sm-4">
+                    <img src="img/ser/p.png">
+                    <h6>WEB DESIGN</h6>
+                    <p>Make better web design than your competitor with simple look but perfect and modern.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="img/ser/vid.png">
+                    <h6>VIDEO MAKER</h6>
+                    <p>Make awesome and professional video to promote your brand, bussines or your company. </p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="img/ser/pnc.png">
+                    <h6>PHOTO EDITOR</h6>
+                    <p>Edit your photos faster and nicely</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Services -->
+
+	<!-- Konten Portfolio -->
+    <section class="portfolio" id="portfolio" >
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-2 text-center">
+                    <h5>PORTFOLIO</h5>
+                    <hr class="line">
+                    <br><br>
+                </div>
+            </div>
+            <div class="row">
+                <div class="column col-sm-4">
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/0051.jpg"> 
+                        <p>-TOKYO-</p>   
+                    </div>
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/0045.jpg"> 
+                        <p>-MIAMI-</p>   
+                    </div>
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/4.jpg"> 
+                        <p>-CAFETERIA-</p>   
+                    </div>
+                </div>
+                <div class="column col-sm-4">
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/2.jpg"> 
+                        <p>-BANDUNG-</p>   
+                    </div>
+                     <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/0090.jpg"> 
+                        <p>-HOKKAIDO-</p>   
+                    </div> 
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/3.jpg"> 
+                        <p>-HONGKONG-</p>   
+                    </div>
+                </div>
+                <div class="column col-sm-4">
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/0011.jpg"> 
+                        <p>-YOGYAKARTA-</p>   
+                    </div>
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/0052.jpg"> 
+                        <p>-LONDON-</p>   
+                    </div>
+                    <div class="cc">
+                        <img class="img-fluid img-tamnel" src="img/port/1.jpg"> 
+                        <p>-KYOTO-</p>   
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </section>
+    <!-- End Portfolio -->
+
+    <!-- Contact -->
+    <section class="contact" id="contact">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-2 text-center">
+                    <h5>CONTACT</h5>
+                    <hr class="line">
+                    <br>
+                </div>  
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-6  text-white">
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Email</label>
+                            <input type="text" id="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Subject</label>
+                            <input type="text" id="subject" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Message</label>
+                            <input type="textarea" id="comment" class="form-control" style="height: 100px">
+                        </div>
+                        <button class="cusbutton" type="submit">SUBMIT</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Contact -->
+
+    <!-- More -->
+    <section class="more" id="more">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-4 text-center">
+                    <h5>Work With Me</h5>
+                    <hr class="line2">
+                    <p>Make your own to be nice and perfect.</p>
+                    <button class="cusbutton" type="submit" style="width: 100px;">HIRE ME</button>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <h5>Follow Me</h5>
+                    <hr class="line2">
+                    <P>sendyapriatna@yahoo.co.id<br>sendyapriatna001@gmail.com<br>+62 857 5911 0587</P>
+                    <a href="https://www.facebook.com/sendyapriatna10/"><img src="img/more/1.png"></a>
+                    <a href="https://www.instagram.com/sndy.prtn/"><img src="img/more/2.png"></a>
+                    <a href=""><img src="img/more/3.png"></a>
+                    <a href=""><img src="img/more/4.png"></a>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <h5>Address</h5>
+                    <hr class="line2">
+                    <p>Dsn Karang Salam RT01/RW01<br>Desa Pananjung,<br>Kec. Pangandaran,<br>Kab. Pangandaran Jawa Barat, 46396</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End More -->
+
+	<!-- Footer -->
+    <footer>
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-sm-12 text-center">
+    				<p>- &copy; Copyright 2020. Portfolio by Sendy Apriatna -</p>
+    			</div>
+    		</div>
+    	</div>
+    </footer>
+    <!-- End Footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.1.0.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
